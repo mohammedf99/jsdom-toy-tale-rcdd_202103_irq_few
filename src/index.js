1b284be56a805abe33676e5b2a401d87be958461
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-06-22 23:17:49 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let addToy = false;
+const addBtn = document.getElementById('new-toy-btn')
+const toyForm = document.querySelector('.container')
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +15,44 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetchToys() {
+   fetch("../db.json")
+  .then(response => response.json())
+  .then(json =>{ 
+    setTimeout( ()=> {  showToys(json.toys); }, 1000)
+   });
+
+}
+
+function showToys(json) {
+  console.log(json)
+  let toyCollection = document.getElementById("toy-collection")
+  json.forEach(element => {
+    let cardDiv = document.createElement("div")
+    cardDiv.className = "card"
+    let button = document.createElement("button")
+    button.className = "like-btn"
+    button.id = element.id
+    button.innerText = "Like Me!"
+    button.addEventListener("click", function(event) {
+      addLikes(event)
+    })
+    let h2 = document.createElement("h2")
+    h2.innerHTML = element.name
+    let img = document.createElement("img")
+    img.src = element.image
+    img.alt = element.name
+    img.className = "toy-avatar"
+    let p = document.createElement("p")
+    p.innerText = `${element.likes} Likes`
+
+    cardDiv.append(h2, img, button, p)
+    toyCollection.appendChild(cardDiv)
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetchToys()
+
+}) 
